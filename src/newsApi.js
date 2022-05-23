260a8b981e6cfb4dea42d07add629b684832f4b5
GregorHorvatH <gregor.horvath83@gmail.com>
2022-05-23 21:44:50 +0300

    heroku

--- src/newsApi.js
+++
@@ -1,6 +0,0 @@
-const URL = 'https://hn.algolia.com/api/v1/search';
-
-export const fetchNews = ({ query, page, hitsPerPage }) =>
-  axios
-    .get(`${URL}?query=${query}&page=${page}&hitsPerPage=${hitsPerPage}`)
-    .then(({ data }) => data);
