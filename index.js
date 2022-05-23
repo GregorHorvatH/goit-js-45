5c6c6e8dd2ea28c8f1ab8222b02f75b37fb59ae8
GregorHorvatH <gregor.horvath83@gmail.com>
2022-05-23 21:47:58 +0300

    heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + './index.html');
 });
 
 io.on('connection', (socket) => {
