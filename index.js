8150d612277805d041a976d75817924f837d06cf
GregorHorvatH <gregor.horvath83@gmail.com>
2022-05-26 16:42:30 +0300

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
-const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
