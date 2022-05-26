0aba3ab3353a78dae2f4bd6465c34b48b211be92
GregorHorvatH <gregor.horvath83@gmail.com>
2022-05-26 21:15:39 +0300

    update chat port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
